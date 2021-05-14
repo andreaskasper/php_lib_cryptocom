08f91b58a11ff086ee04448b91b12431dfd0c852
Andreas Kasper <djassi@web.de>
2021-05-14 19:59:07 +0200

    Switching Currencies base / quote

--- src/Common.php
+++ src/Common.php
@@ -10,8 +10,8 @@ class Common {
         if (!empty($depth)) $w["depth"] = $depth;
         $data = core::request("public/get-book", $w);
         //print_r($data); exit;
-        foreach ($data["result"]["data"][0]["bids"] as $row) $out["bids"][] = array("price" => new Price($row[0], $instrument->base->id), "quantity" => new Quantity($row[1], $instrument->quote->id), "count" => $row[2]);
-        foreach ($data["result"]["data"][0]["asks"] as $row) $out["asks"][] = array("price" => new Price($row[0], $instrument->base->id), "quantity" => new Quantity($row[1], $instrument->quote->id), "count" => $row[2]);
+        foreach ($data["result"]["data"][0]["bids"] as $row) $out["bids"][] = array("price" => new Price($row[0], $instrument->quote->id), "quantity" => new Quantity($row[1], $instrument->base->id), "count" => $row[2]);
+        foreach ($data["result"]["data"][0]["asks"] as $row) $out["asks"][] = array("price" => new Price($row[0], $instrument->quote->id), "quantity" => new Quantity($row[1], $instrument->base->id), "count" => $row[2]);
         $out["instrument"] = $instrument;
         $out["timestamp"] = new \DateTime("@".($data["result"]["data"][0]["t"]/1000));
         return $out;
