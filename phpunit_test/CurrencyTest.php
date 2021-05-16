7184b5e2d80e5f037707e56f360e8cb66a6564b2
Andreas Kasper <djassi@web.de>
2021-05-16 14:20:41 +0200

    Create CurrencyTest.php

---
+++ phpunit_test/CurrencyTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ *
+ *
+ *
+ * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+namespace cryptocom\Test;
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * crypto.com - Currency tester
+ */
+final class CurrencyTest extends TestCase {
+
+	public function testcurrencies() {
+    $curs = array("BTC", "XRP", "CRO");
+    foreach ($curs as $a) {
+      $currency = new Currency($a);
+      $this->assertEquals(true, is_string($currency->id));
+      $this->assertEquals(true, is_string($currency->string));
+    }
+	}
+
+	
+	
+}
